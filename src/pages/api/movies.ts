7ff46dc5d0a704fdd404af8ee537b5196d66f41c
Francis Lagares <francis.lagares@gmail.com>
2022-08-21 20:07:34 +0200

    feat: create movies api route

---
+++ src/pages/api/movies.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { POPULAR_BASE_URL, SEARCH_BASE_URL } from 'src/config/config';
+import { genericFetch } from 'src/services/fetchService';
+import { IMovies } from 'src/types/types';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<IMovies>,
+) {
+  const { page, search } = req.query;
+
+  const endpoint = search
+    ? `${SEARCH_BASE_URL}${search}&page=${page}`
+    : `${POPULAR_BASE_URL}&page=${page}`;
+
+  const data = await genericFetch<IMovies>(endpoint);
+
+  res.status(200).json(data);
+}

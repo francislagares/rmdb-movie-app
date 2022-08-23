3858b9676088c90fee40dd1bd2821aec99866a9a
Francis Lagares <francis.lagares@gmail.com>
2022-08-23 21:00:01 +0200

    feat: create custom tailwind property

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    gridTemplateColumns: {
+      'auto-fill': 'repeat(auto-fill, minmax(200px, 1fr))',
+    },
     extend: {
       height: {
         128: '40rem',
